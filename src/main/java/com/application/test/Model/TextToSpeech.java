c6720a912f2555847e06e83f11684843cbd46ee2
TrinhTuanHai <24022318@vnu.edu.vn>
2025-05-06 07:26:51 +0700

    added tts (app can talk POGGGGGG!!!!!)

---
+++ src/main/java/com/application/test/Model/TextToSpeech.java
@@ -0,0 +1,46 @@
+package com.application.test.Model;
+
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class TextToSpeech {
+    public static void speak(String text) throws Exception {
+        try {
+            String encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8);
+            String urlStr = "https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=" + encodedText;
+
+            File tmpFile = File.createTempFile("tts", ".wav");
+            tmpFile.deleteOnExit();
+
+            try (InputStream in = new URL(urlStr).openStream()) {
+                Files.copy(in, tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
+
+            // Play using JavaFX MediaPlayer
+            Media media = new Media(tmpFile.toURI().toString());
+            MediaPlayer mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.setOnError(() -> {
+                System.err.println("MediaPlayer error: " + mediaPlayer.getError().getMessage());
+            });
+            mediaPlayer.play();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
